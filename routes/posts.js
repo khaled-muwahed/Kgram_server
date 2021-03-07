7e67361c1135a987f5b4c500c7446203a284388c
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-03-07 22:48:45 +0000

    bug fixes

--- routes/posts.js
+++ routes/posts.js
@@ -4,11 +4,11 @@ const User = require('../model/User');
 const router = require ('express').Router();
 const authotise = require('./verifyAuth');
 router.get('/',authotise, (req, res) => {
-/*res.json({posts:
+res.json({posts:
      {title: 'my first post',
      des: 'random data'}
     });
-*/
+
 //const find = User.findOne({_id: req.user});
 res.send(req.user);
 
