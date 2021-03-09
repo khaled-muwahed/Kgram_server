dfc6dffeaf54582f7c6fc8368f3009ed569e5268
khaled-muwahed <52414740+khaled-muwahed@users.noreply.github.com>
2021-03-09 17:34:31 +0000

    post image

--- index.js
+++ index.js
@@ -6,28 +6,37 @@ const authRoute = require('./routes/auth');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const errorHandler = require('./errorHandling/server_error_handler');
+var bodyParser = require('body-parser');
+
+
 
 const postRoute = require('./routes/posts');
 //const router = require('./routes/auth');
 dotenv.config();
 //connect to db
 
- mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true },
-   { useUnifiedTopology: true } , 
-    () => console.log('db connected !!!!!!!!!!!!!!!!!!!!')
+ mongoose.connect(process.env.DB_CONNECT ,
+    { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    console.log('connected')
+}
 );
- 
- /*catch(err) {
-     console.log("connection error " , err);
 
- }*/
 //Middleware
 app.use(express.json());
 //app.use(errorHandler);
 //Route Middleware
 app.use('/api/user', authRoute);
 
+//making storage folder publicly accessible 
+app.use('/Storage', express.static('Storage'))
+
 //check user auth
 app.use('/api/posts', postRoute);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+ 
+// Set EJS as templating engine 
+app.set("view engine", "ejs");
 
 app.listen(3000, () => console.log('server up and running'))
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,89 @@
 const { Router } = require('express');
 const User = require('../model/User');
 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+destination: function(req , file , cb) {
+    cb(null,'./storage/')
+},
+filename: function  (req , file, cb) {
+    cb (null, new Date().toISOString() + file.originalname);
+    
+}
+});
+
+
+
+
+
+const upload = multer({storage : storage})
+
 const router = require ('express').Router();
 const authotise = require('./verifyAuth');
-router.get('/',authotise, (req, res) => {
-res.json({posts:
-     {title: 'my first post',
-     des: 'random data'}
+const imgModel = require('../model/postModel'); 
+const { Mongoose } = require('mongoose');
+
+var path = require('path');
+
+
+
+router.post('/', upload.single('image'),authotise, (req, res , next) => {
+    const Img = new imgModel ({
+       
+        caption: req.body.caption,
+        img: {
+            
+            data: fs.readFileSync(path.join(__dirname + '/storage/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+        
+    });
+    console.log(req.file)
+    
+
+    Img.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "succsess"
+        })
+    })
+
+    
+    .catch((err) => {
+        console.log(err , "eroororoorroor")
     });
 
 //const find = User.findOne({_id: req.user});
-res.send(req.user);
-
+    res.send(Img);
 }
 
 );
+
+var fs = require('fs');
+//var path = require('path');
+/*
+
+router.post('/', upload.single('image'), (req, res, next) => {
+ 
+    var obj = {
+        caption: req.body.caption,
+        img: {
+            
+            data: fs.readFileSync(path.join(__dirname + '/storage/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    imgModel.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+             item.save();
+            res.redirect('/');
+        }
+    });
+});*/
 
 module.exports = router;
